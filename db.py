c8f8a1c74b15865e24d0bf867b6cf66a98f7c494
ProdiGy247 <105563522+ProdiGy247@users.noreply.github.com>
2024-06-01 16:03:59 -0700

    Update db.py

--- db.py
+++ db.py
@@ -1,41 +1,29 @@
-import sqlite3
+from supabase import create_client, Client
 import pandas as pd
 
-# Crear la base de datos y la tabla si no existen
-def crear_tabla():
-    conn = sqlite3.connect('salon.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS clientes (
-            id INTEGER PRIMARY KEY,
-            nombre TEXT,
-            servicio TEXT,
-            costo REAL,
-            fecha TEXT,
-            atendido_por TEXT,
-            formula_tinte TEXT
-        )
-    ''')
-    conn.commit()
-    conn.close()
 
-# Función para insertar datos en la tabla
-def insertar_cliente(nombre, servicio, costo, fecha, atendido_por, formula_tinte):
-    conn = sqlite3.connect('salon.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO clientes (nombre, servicio, costo, fecha, atendido_por, formula_tinte)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', (nombre, servicio, costo, fecha, atendido_por, formula_tinte))
-    conn.commit()
-    conn.close()
+# Configuración de Supabase
+SUPABASE_URL = 'https://lrwufbjvkfnyfjyjuzue.supabase.co'
+SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyd3VmYmp2a2ZueWZqeWp1enVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcyNzUyNDQsImV4cCI6MjAzMjg1MTI0NH0.OI88wwe7zTNLMdmaQBW7TRjoK3cU0Mx3koFs0Sam52Q'
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Función para obtener todos los datos de la tabla
-def obtener_datos():
-    conn = sqlite3.connect('salon.db')
-    df = pd.read_sql_query("SELECT * FROM clientes", conn)
-    conn.close()
-    return df
 
-# Ejecutar la función para crear la tabla al importar el módulo
-crear_tabla()
+
+def obtener_datos():
+    response = supabase.table('clientes').select('*').execute()
+    return pd.DataFrame(response.data)
+
+# Función para insertar datos en la tabla
+
+
+def insertar_cliente(nombre, servicio, costo, fecha, atendido_por, formula_tinte, celular):
+    supabase.table('clientes').insert({
+        'nombre': nombre,
+        'servicio': servicio,
+        'costo': costo,
+        'fecha': fecha,
+        'atendido_por': atendido_por,
+        'formula_tinte': formula_tinte,
+        'celular': celular
+    }).execute()
