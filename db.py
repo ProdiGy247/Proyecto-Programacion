c55d02a23fd822bc92c165b44375a091876f63de
ProdiGy247 <105563522+ProdiGy247@users.noreply.github.com>
2024-06-03 18:46:23 -0700

    db.py

--- db.py
+++ db.py
@@ -4,25 +4,9 @@ import pandas as pd
 
 
 # Configuración de Supabase
-SUPABASE_URL = os.environ.get('https://lrwufbjvkfnyfjyjuzue.supabase.co')
-SUPABASE_KEY = os.environ.get('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyd3VmYmp2a2ZueWZqeWp1enVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcyNzUyNDQsImV4cCI6MjAzMjg1MTI0NH0.OI88wwe7zTNLMdmaQBW7TRjoK3cU0Mx3koFs0Sam52Q')
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-def crear_tabla():
-    query = '''
-    CREATE TABLE IF NOT EXISTS clientes (
-        id SERIAL PRIMARY KEY,
-        nombre TEXT NOT NULL,
-        servicio TEXT NOT NULL,
-        costo REAL NOT NULL,
-        fecha DATE NOT NULL,
-        atendido_por TEXT NOT NULL,
-        formula_tinte TEXT NOT NULL,
-        celular TEXT NOT NULL
-    )
-    '''
-    response = supabase.rpc('execute_sql', {'sql': query}).execute()
-
+url: str = os.environ.get("https://lrwufbjvkfnyfjyjuzue.supabase.co")
+key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyd3VmYmp2a2ZueWZqeWp1enVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcyNzUyNDQsImV4cCI6MjAzMjg1MTI0NH0.OI88wwe7zTNLMdmaQBW7TRjoK3cU0Mx3koFs0Sam52Q")
+supabase: Client = create_client(url, key)
 
 # Función para obtener todos los datos de la tabla
 
@@ -35,18 +19,12 @@ def obtener_datos():
 
 
 def insertar_cliente(nombre, servicio, costo, fecha, atendido_por, formula_tinte, celular):
-    try:
-        data = {
-            "nombre": nombre,
-            "servicio": servicio,
-            "costo": costo,
-            "fecha": fecha,
-            "atendido_por": atendido_por,
-            "formula_tinte": formula_tinte,
-            "celular": celular
-        }
-        response = supabase.table('clientes').insert(data).execute()
-        if response.status_code != 201:
-            raise Exception(f"Error al insertar en la base de datos: {response.data}")
-    except Exception as e:
-        print(f"Error al insertar en la base de datos: {e}")
+    supabase.table('clientes').insert({
+        'nombre': nombre,
+        'servicio': servicio,
+        'costo': costo,
+        'fecha': fecha,
+        'atendido_por': atendido_por,
+        'formula_tinte': formula_tinte,
+        'celular': celular
+    }).execute()
